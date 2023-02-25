5c83988f59bb3ba781b20783d870246a451cc5f8
Anrat-dev <anurati.bhosekar@gmail.com>
2023-02-25 15:31:46 -0500

    Tables uplaoded to admin, created 1 super user

--- eatdapoints/admin.py
+++ eatdapoints/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Meal, Calories, GameType, UserGameType, UserMeals ,UserCalories, UserPoints
+
+admin.site.register(Meal)
+admin.site.register(Calories)
+admin.site.register(GameType)
+admin.site.register(UserGameType)
+admin.site.register(UserMeals)
+admin.site.register(UserCalories)
+admin.site.register(UserPoints)
