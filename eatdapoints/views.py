8f61d498ecf1c1f3e89f605cf2fb12c73ca0bb4c
Gabriela Betancourt <gabrielabetancourt@vl965-172-31-104-196.wireless.umass.edu>
2023-02-25 16:35:34 -0500

    front end basic stuff

--- eatdapoints/views.py
+++ eatdapoints/views.py
@@ -1,30 +1,10 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 import datetime
 from django.db.models import Avg, Count, Min, Sum
 from .models import Meal, GameType, Calories, UserMeals, UserPoints, UserCalories, UserGameType
 
-# Create your views here.
-
-def home_page(request):
-    user_points = UserPoints.objects.filter(player=request.user).aggregate(Sum('points'))
-    user_meals = UserMeals.objects.filter(player=request.user, meal_date=datetime.date.today).count()
-    user_meals_all = UserMeals.objects.filter(player=request.user).count()
-    user_calories = UserCalories.objects.filter(player=request.user, meal_date=datetime.date.today).aggregate(Sum('calories'))
-    acct = UserGameType.objects.get(player=request.user)
-    acct_type = acct.game_type
-
-    return render(request, 'eatdapoints/home_page.html', {'user_points': user_points, 'user_meals': user_meals, 'user_meals_all': user_meals_all, 'user_calories': user_calories, 'acct_type': acct_type})
-
-
-def meal_list(request):
-    user_meal_list = UserMeals.objects.filter(player=request.user).order_by('-meal_date')
-    user_meals_count = UserMeals.objects.filter(player=request.user).count()
-
-    return render(request, 'eatdapoints/meal_list.html', {'user_meal_list': user_meal_list, 'user_meals_count': user_meals_count})
-
-
-def point_list(request):
-    user_points_list = UserPoints.objects.filter(player=request.user).order_by('-meal_date')
-    user_points_tot = UserPoints.objects.filter(player=request.user).aggregate(Sum('points'))
-
-    return render(request, 'eatdapoints/point_list.html', {'user_points_list': user_points_list, 'user_points_tot': user_points_tot})
+def home(request):
+  template = loader.get_template('homepage.html')
+  return HttpResponse(template.render())
